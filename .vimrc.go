081063f252fbdaf2b051aaa9321156bdb810a350
Shiqi Wang <shiqiwang308@gmail.com>
2020-10-10 22:35:07 -0400

    update

---
+++ .vimrc.go
@@ -0,0 +1,11 @@
+set runtimepath^="/home/shiqi/.vim_go_runtime"
+
+source /home/shiqi/.vim_go_runtime/vimrc/basic.vim
+source /home/shiqi/.vim_go_runtime/vimrc/filetypes.vim
+source /home/shiqi/.vim_go_runtime/vimrc/plugins.vim
+source /home/shiqi/.vim_go_runtime/vimrc/extended.vim
+
+try
+  source /home/shiqi/.vim_go_runtime/custom_config.vim
+catch
+endtry
